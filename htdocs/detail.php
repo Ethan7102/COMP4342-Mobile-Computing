3601faf3fcac382c87484dd10c3b4c1f2d9f1bd9
thomaslee3156 <79982174+thomaslee3156@users.noreply.github.com>
2021-03-15 04:21:38 +0800

    Update detail.php

--- htdocs/detail.php
+++ htdocs/detail.php
@@ -5,7 +5,7 @@
 	
 	while ($row = $result->fetch_assoc())
     {
-        $output[] = $row; // 就逐項將回傳的東西放到陣列中
+        $output[] = $row;
     }
 	echo json_encode($output, JSON_UNESCAPED_UNICODE);
 	$link->close();
