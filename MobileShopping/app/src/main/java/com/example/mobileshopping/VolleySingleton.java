48568a6e2a2dccef8e05c435e398106a1675184b
thomaslee3156 <79982174+thomaslee3156@users.noreply.github.com>
2021-04-02 18:28:32 +0800

    volley singleton
    you can use
    private RequestQueue queue;
    queue = VolleySingleton.getInstance(this.getApplicationContext()).getRequestQueue();

---
+++ MobileShopping/app/src/main/java/com/example/mobileshopping/VolleySingleton.java
@@ -0,0 +1,31 @@
+package com.example.mobileshopping;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class VolleySingleton {
+    private static com.example.mobileshopping.VolleySingleton instance;
+    private RequestQueue requestQueue;
+    private static Context ctx;
+
+    private VolleySingleton(Context context) {
+        ctx = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized com.example.mobileshopping.VolleySingleton getInstance(Context context) {
+        if (instance == null) {
+            instance = new com.example.mobileshopping.VolleySingleton(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+}
