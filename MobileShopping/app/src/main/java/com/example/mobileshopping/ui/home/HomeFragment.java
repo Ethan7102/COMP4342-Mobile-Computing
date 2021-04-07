5225560c855bd7bb7f5db08720a41d0a50e9647f
Ethan <51076587+Ethan7102@users.noreply.github.com>
2021-04-07 13:10:16 +0800

    support displaying products on network access failure

--- MobileShopping/app/src/main/java/com/example/mobileshopping/ui/home/HomeFragment.java
+++ MobileShopping/app/src/main/java/com/example/mobileshopping/ui/home/HomeFragment.java
@@ -1,7 +1,11 @@
 package com.example.mobileshopping.ui.home;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,6 +24,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.mobileshopping.Product;
 import com.example.mobileshopping.R;
 import com.google.android.material.navigation.NavigationView;
 
@@ -28,21 +33,37 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Timer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    HomeViewModel homeViewModel;
     Button btnSearch;
     EditText edSearch;
     ListView lvProducts;
     String[] productList;
+    Product[] productObjList;
+    HttpURLConnection con;
+    JSONObject jObj;
+    JSONArray products;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -65,18 +86,30 @@ public class HomeFragment extends Fragment {
         InputStream inputStream = null;
         String result = "";
         URL url = null;
-
         try {
             //specific ip address
             url = new URL("http://192.168.1.11/webServer/COMP4342-Mobile-Computing/getProducts.php");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
-            Log.d("doInBackground", "process start");
+            Log.d("connectServer", "process start");
 
             //not async
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
-            con.connect();
+
+            ExecutorService service = Executors.newSingleThreadExecutor();
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        con.connect();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            Future<?> f = service.submit(r);
+            f.get(2, TimeUnit.SECONDS); //if the connection cannot completes with in 2 seconds, a TimeroutException will be throw.
 
             inputStream = con.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
@@ -84,24 +117,53 @@ public class HomeFragment extends Fragment {
             while ((line = bufferedReader.readLine()) != null) {
                 result += line;
             }
-            Log.d("doInBackground", "process complete");
+            Log.d("connectServer", "process complete");
             inputStream.close();
 
             //process JSON
-            JSONObject jObj = new JSONObject(result);
-            JSONArray products = jObj.getJSONArray("products");
-            productList=new String[products.length()];
-                for (int i = 0; i < products.length(); i++) {
-                    JSONObject product = products.getJSONObject(i);
-                    productList[i] = product.getString("brand") + "\n" + product.getString("productName") + "\nHK$" + product.getString("price");
-                }
-            ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,productList);
-            lvProducts.setAdapter(adapter);
-            lvProducts.setOnItemClickListener(onClickListView);
+            jObj = new JSONObject(result);
+            products = jObj.getJSONArray("products");
+            productList = new String[products.length()];
+            productObjList = new Product[products.length()];
+            for (int i = 0; i < products.length(); i++) {
+                JSONObject product = products.getJSONObject(i);
+                productList[i] = product.getString("brand") + "\n" + product.getString("productName") + "\nHK$" + product.getString("price");
+            }
+
+            //save product list
+            SharedPreferences sharedPref = getActivity().getSharedPreferences("appData", Context.MODE_PRIVATE);
+            SharedPreferences.Editor prefEditor = getActivity().getSharedPreferences("appData", Context.MODE_PRIVATE).edit();
+            prefEditor.putString("jsonProductList", result);
+            prefEditor.commit();
+
+
         } catch (Exception e) {
             e.printStackTrace();
             String msg = e.getMessage();
-            System.out.println("error: "+msg);
+            System.out.println("error: " + msg);
+
+            //get products from localstorage
+            SharedPreferences sharedPreferences = getActivity().getSharedPreferences("appData", Context.MODE_PRIVATE);
+            String str = sharedPreferences.getString("jsonProductList", "null");
+            if (str != "null") {
+                System.out.println(str);
+                try {
+                    jObj = new JSONObject(str);
+                    products = jObj.getJSONArray("products");
+                    productList = new String[products.length()];
+                    for (int i = 0; i < products.length(); i++) {
+                        JSONObject product = products.getJSONObject(i);
+                        productList[i] = product.getString("brand") + "\n" + product.getString("productName") + "\nHK$" + product.getString("price");
+                    }
+                } catch (JSONException jsonException) {
+                    jsonException.printStackTrace();
+                }
+            }
+        }
+        if (productList != null) {
+            ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, productList);
+            lvProducts.setAdapter(adapter);
+            lvProducts.setOnItemClickListener(onClickListView);
         }
         return root;
     }
