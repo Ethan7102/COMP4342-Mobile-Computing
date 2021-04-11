6effb012d394625cfa0afa2cba68ac916dd68a9f
thomaslee3156 <79982174+thomaslee3156@users.noreply.github.com>
2021-04-12 04:18:11 +0800

    create order

--- MobileShopping/app/src/main/java/com/example/mobileshopping/ui/dashboard/CreateOrder.java
+++ MobileShopping/app/src/main/java/com/example/mobileshopping/ui/dashboard/CreateOrder.java
@@ -26,7 +26,7 @@ import java.util.Map;
 public class CreateOrder extends AppCompatActivity {
     TextView tv_email, tv_code, tv_status;
     private RequestQueue queue;
-    String url="http://192.168.1.5/getCartProduct.php";
+    String url="http://192.168.1.5/sendOrder.php";
     String email;
     SharedPreferences cart;
     @Override
@@ -34,6 +34,9 @@ public class CreateOrder extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         queue = VolleySingleton.getInstance(getApplicationContext()).getRequestQueue();
         setContentView(R.layout.activity_create_order);
+        tv_status=findViewById(R.id.tv_status);
+        tv_email=findViewById(R.id.tv_email);
+        tv_code=findViewById(R.id.tv_code);
         Intent intent=getIntent();
         email=intent.getStringExtra("email");
         cart = getSharedPreferences("shopping_cart", MODE_PRIVATE);
@@ -44,9 +47,16 @@ public class CreateOrder extends AppCompatActivity {
             @Override
             public void onResponse(String response) {
                 try {
+                    Log.i("response", response.toString());
                     JSONObject inform = new JSONObject(response);
+                    tv_email.setText(inform.getString("email"));
+                    tv_code.setText(inform.getString("code"));
+                    tv_status.setText("Success!, The order has been accepted, keep the email and confirmation code to check the order");
                 } catch (JSONException e) {
                     e.printStackTrace();
+                    tv_email.setText("");
+                    tv_code.setText("Error");
+                    tv_status.setText("Failed!, Something happened.\nPlease try again later");
                 }
             }
 
@@ -63,7 +73,7 @@ public class CreateOrder extends AppCompatActivity {
                 int i=0;
                 for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
                     try {
-                        jsonObject.put(""+i, Integer.valueOf(entry.getKey().toString()));
+                        jsonObject.put(entry.getKey(), entry.getValue());
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
