a61c6d7707bd8ae3b600162bf8b01f1c2c886f33
tiffanyyyy <pangchinwing@gmail.com>
2021-04-20 02:34:02 +0800

    edit order

--- MobileShopping/app/src/main/java/com/example/mobileshopping/ui/notifications/NotificationsFragment.java
+++ MobileShopping/app/src/main/java/com/example/mobileshopping/ui/notifications/NotificationsFragment.java
@@ -24,9 +24,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.mobileshopping.APIUrl;
 import com.example.mobileshopping.R;
 import com.google.android.material.button.MaterialButton;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -45,9 +47,11 @@ import java.util.concurrent.TimeUnit;
 public class NotificationsFragment extends Fragment {
 
     TextView txtEmail;
+    TextView txtCode;
     ListView lvOrder;
     MaterialButton btnConfirm;
     HttpURLConnection con;
+    JSONArray orders;
 
 
     private NotificationsViewModel notificationsViewModel;
@@ -66,6 +70,7 @@ public class NotificationsFragment extends Fragment {
         });
 
         txtEmail = root.findViewById(R.id.txtEmail);
+        txtCode = root.findViewById(R.id.txtCode);
         lvOrder = root.findViewById(R.id.lvOrder);
         btnConfirm = root.findViewById(R.id.btnConfirm);
 
@@ -73,19 +78,58 @@ public class NotificationsFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 String email = txtEmail.getText().toString();
+                String code = txtCode.getText().toString();
                 System.out.println(email);
 
-                String mailto = "mailto:" + email +
-                        "?cc=" +
-                        "&subject=" + Uri.encode("your subject") +
-                        "&body=" + Uri.encode("your mail body");
-                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
-                emailIntent.setData(Uri.parse(mailto));
+                URL url = null;
+                InputStream inputStream = null;
+                String result = "";
 
                 try {
-                    startActivity(emailIntent);
-                } catch (ActivityNotFoundException e) {
-                    Toast.makeText(getContext(), "Error to open email app", Toast.LENGTH_SHORT).show();
+                    url = new URL(APIUrl.url+"/orderDetail.php");
+                    con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("GET");
+                    Log.d("connectServer", "process start");
+
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+
+                    ExecutorService service = Executors.newSingleThreadExecutor();
+                    Runnable r = new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                con.connect();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+
+                    Future<?> f = service.submit(r);
+                    f.get(1, TimeUnit.SECONDS);
+
+                    inputStream = con.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                    String line = "";
+                    while ((line = bufferedReader.readLine()) != null) {
+                        result += line;
+                    }
+                    Log.d("connectServer", "process complete");
+                    inputStream.close();
+
+                    JSONObject responseJSON = new JSONObject(result);
+                    orders = responseJSON.getJSONArray("products");
+                    for(int i = 0; i < orders.length(); i++){
+                        JSONObject order = orders.getJSONObject(i);
+                        String productName = order.optString("productName");
+                        String price = order.optString("price");
+                        String quantity = order.optString("quantity");
+
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
                 }
             }
         });
@@ -93,44 +137,6 @@ public class NotificationsFragment extends Fragment {
         URL url = null;
         InputStream inputStream = null;
         String result = "";
-
-        try{
-            url = new URL("http://192.168.1.11/webServer/COMP4342-Mobile-Computing/orderDetail.php");
-            con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            Log.d("connectServer", "process start");
-
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-
-            ExecutorService service = Executors.newSingleThreadExecutor();
-            Runnable r = new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        con.connect();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-            Future<?> f = service.submit(r);
-            f.get(1, TimeUnit.SECONDS); //if the connection cannot completes with in 1 seconds, a TimeroutException will be throw.
-
-            inputStream = con.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            String line = "";
-            while ((line = bufferedReader.readLine()) != null) {
-                result += line;
-            }
-            Log.d("connectServer", "process complete");
-            inputStream.close();
-
-
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
 
 
 
