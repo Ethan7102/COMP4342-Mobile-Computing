c41e671f7438e9f2f32ba40cef30cfe9988c3a6d
thomaslee3156 <79982174+thomaslee3156@users.noreply.github.com>
2021-04-23 15:57:10 +0800

    update

--- htdocs/orderDetail.php
+++ htdocs/orderDetail.php
@@ -4,11 +4,17 @@
 	$sql="SELECT product.productID, product.productName, product.price, orderdetail.quantity From orderdetail Inner Join product ON product.productID=orderdetail.productID WHERE orderdetail.orderID=(SELECT orderID FROM `order` WHERE email='$email' AND confirmationCode='$code')";
 	
 	$result=$link->query($sql) or die($link->error);
-	
 	while ($row = $result->fetch_assoc())
     {
         $output[] = $row;
     }
-	echo json_encode($output, JSON_UNESCAPED_UNICODE);
+	if(isset($output)) {
+		$json = ["status" => "success", "product" => $output];
+		echo json_encode($json, JSON_UNESCAPED_UNICODE);
+	} else {
+		$status = "confirmation code or email error";
+		$json = ["status" => $status];
+		echo json_encode($json, JSON_UNESCAPED_UNICODE);
+	}
 	$link->close();
 ?>
