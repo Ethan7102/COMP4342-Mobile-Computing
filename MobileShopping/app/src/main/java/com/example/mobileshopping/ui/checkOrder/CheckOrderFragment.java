c9cd1cd1714c31d97c07e58c87facee6b098173f
Ethan <51076587+Ethan7102@users.noreply.github.com>
2021-04-25 00:49:44 +0800

    Update CheckOrderFragment.java

--- MobileShopping/app/src/main/java/com/example/mobileshopping/ui/checkOrder/CheckOrderFragment.java
+++ MobileShopping/app/src/main/java/com/example/mobileshopping/ui/checkOrder/CheckOrderFragment.java
@@ -1,10 +1,12 @@
 package com.example.mobileshopping.ui.checkOrder;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -22,6 +24,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.example.mobileshopping.APIUrl;
 import com.example.mobileshopping.R;
+import com.example.mobileshopping.VolleySingleton;
 import com.google.android.material.button.MaterialButton;
 
 import org.json.JSONArray;
@@ -42,11 +45,12 @@ public class CheckOrderFragment extends Fragment {
     ListView lvOrderDetail;
     MaterialButton btnConfirm;
     HttpURLConnection con;
-    JSONArray orders;
+    JSONArray orderDetail;
     String[] orderDetailList;
     private RequestQueue queue;
     String url = APIUrl.url+"/orderDetail.php";
     private CheckOrderViewModel checkOrderViewModel;
+    double amount;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -60,7 +64,7 @@ public class CheckOrderFragment extends Fragment {
                 //textView.setText(s);
             }
         });
-
+        queue = VolleySingleton.getInstance(getActivity().getApplicationContext()).getRequestQueue();
         txtEmail = root.findViewById(R.id.txtEmail);
         txtCode = root.findViewById(R.id.txtCode);
         lvOrderDetail = root.findViewById(R.id.lvOrder);
@@ -73,41 +77,45 @@ public class CheckOrderFragment extends Fragment {
                 String code = txtCode.getText().toString();
                 System.out.println(email+"  "+code);
 
-                InputStream inputStream = null;
-                String result = "";
-                orderDetailList = new String[10];
-                orderDetailList[0] = "Product Name\t\tQty\tPrice\n";
                 StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         Log.i("response", response);
+                        amount=0;
                         try {
-                            JSONObject product = new JSONObject(response);
                             System.out.println(response);
-                            /*
-                            productName = product.getString("productName");
-                            productDescription = product.getString("productDescription");
-                            type = product.getString("type");
-                            price = product.getInt("price");
-                            quantity = product.getInt("quantity");
-                            tv_name.setText(productName);
-                            tv_price.setText("$" + price);
-                            tv_type.setText(type);
-                            tv_description.setText(productDescription);
-                            if(quantity>0) {
-                                addCart.setEnabled(true);
-                                addCart.setText(R.string.add_to_cart);
-                            } else {
-                                addCart.setEnabled(false);
-                                quantityOfCart=0;
-                                tv_quantityOfCart.setText(String.valueOf(quantityOfCart));
-                                addCart.setText(R.string.no_stone);
-                            }*/
+                            JSONObject responseJSON = new JSONObject(response);
+                            orderDetail = responseJSON.getJSONArray("product");
+                            orderDetailList = new String[orderDetail.length()+3];
+                            String productName="Product Name";
+                            String qty="Qty";
+                            String price = "Price";
+                            String str = String.format("%-50s %8s %5s", productName,price, qty);
+                            orderDetailList[0] = str;
+                            System.out.println(orderDetail.length());
+                            for(int i = 0; i < orderDetail.length(); i++){
+                                JSONObject product = orderDetail.getJSONObject(i);
+                                productName = product.optString("productName");
+                                System.out.println("productName: "+productName);
+                                price = "$" + product.optString("price");
+                                amount+=product.optDouble("price")*product.optInt("quantity");
+                                qty = product.optString("quantity");
+                                str = String.format("%-50s %8s %5s", productName,price,qty);
+                                orderDetailList[i+1] = str;
+                            }
+                            str = "Total amount: $"+amount;
+                            orderDetailList[orderDetail.length()+1] = str;
+                            str = "Order status:";
+                            orderDetailList[orderDetail.length()+2] = str;
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
+                        if (orderDetailList != null) {
+                            ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, orderDetailList);
+                            lvOrderDetail.setAdapter(adapter);
+                        }
+                        ((InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(txtCode.getWindowToken(), 0);
                     }
-
                 }, (VolleyError error) -> {
                     error.printStackTrace();
                     //tv_name.setText("That didn't work!");
@@ -116,8 +124,8 @@ public class CheckOrderFragment extends Fragment {
                     @Override
                     protected Map<String,String> getParams(){
                         Map<String,String> params = new HashMap<>();
-                        params.put("email",String.valueOf(email));
-                        params.put("code",String.valueOf(code));
+                        params.put("email",email);
+                        params.put("code",code);
                         return params;
                     }
                 };
@@ -125,61 +133,6 @@ public class CheckOrderFragment extends Fragment {
                 queue.add(stringRequest);
 
 
-                /*try {
-                    url = new URL(APIUrl.url+"/orderDetail.php");
-                    con = (HttpURLConnection) url.openConnection();
-                    con.setRequestMethod("GET");
-                    Log.d("connectServer", "process start");
-
-                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                    StrictMode.setThreadPolicy(policy);
-
-                    ExecutorService service = Executors.newSingleThreadExecutor();
-                    Runnable r = new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                con.connect();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    };
-
-                    Future<?> f = service.submit(r);
-                    f.get(1, TimeUnit.SECONDS);
-
-                    inputStream = con.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                    String line = "";
-                    while ((line = bufferedReader.readLine()) != null) {
-                        result += line;
-                    }
-                    //Toast.makeText(getActivity(), result, Toast.LENGTH_SHORT).show();
-
-                    Log.d("connectServer", "process complete");
-                    inputStream.close();
-
-                    JSONObject responseJSON = new JSONObject(result);
-                    orders = responseJSON.getJSONArray("products");
-                    orderDetailList[0] = "Product Name\t\tQty\tPrice\n";
-                    for(int i = 1; i < orders.length(); i++){
-                        JSONObject order = orders.getJSONObject(i);
-                        String productName = order.optString("productName");
-                        String price = order.optString("price");
-                        String quantity = order.optString("quantity");
-                        orderDetailList[i] = "Product Name: " + productName + "\nPrice: " + price + "\nQuantity: " + quantity;
-                    }
-                }
-                catch (Exception e){
-                    e.printStackTrace();
-                }
-
-                 */
-                if (orderDetailList != null) {
-                    ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, orderDetailList);
-                    lvOrderDetail.setAdapter(adapter);
-                }
             }
         });
 
